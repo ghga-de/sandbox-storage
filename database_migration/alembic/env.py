3193e5f3bbb2db0f970c570d69cb8acadf4e0c19
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-06-26 02:54:30 +0000

    Added env variable for db_url

--- database_migration/alembic/env.py
+++ database_migration/alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -12,6 +13,8 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
+config.set_main_option('sqlalchemy.url', os.getenv('DB_URL'))
+
 
 # add your model's MetaData object here
 # for 'autogenerate' support
