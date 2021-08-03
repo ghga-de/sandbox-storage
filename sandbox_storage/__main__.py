a528eefeffbb93514ecc1d1297ae89cd450b2739
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-08-03 08:49:25 +0000

    Flake 8

--- sandbox_storage/__main__.py
+++ sandbox_storage/__main__.py
@@ -1,5 +1,6 @@
 # Copyright 2021 Universität Tübingen, DKFZ and EMBL
 # for the German Human Genome-Phenome Archive (GHGA)
+
 """Entrypoint for the Package"""
 
 from wsgiref.simple_server import make_server
@@ -9,11 +10,13 @@ from .api import get_app
 app = get_app()
 settings = get_settings()
 
+
 def run():
     """Starts backend server"""
 
     server = make_server(settings.host, settings.port, app)
     server.serve_forever()
+
 
 if __name__ == "__main__":
     run()
