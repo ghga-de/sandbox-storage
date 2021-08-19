6df624796182826763eef0e344fb7f850431b98b
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-08-19 07:45:00 +0000

    Script now contained in function

--- sandbox_storage/scripts/populate.py
+++ sandbox_storage/scripts/populate.py
@@ -5,16 +5,15 @@
 # So, just take example files and hard links right now
 
 from datetime import datetime
-from sandbox_storage.database import get_session
-from sandbox_storage.config import get_settings
-from sandbox_storage.models import DrsObject
-
-from sqlalchemy.exc import IntegrityError
 from os import listdir
 from os.path import isfile, join, getsize, getctime
-import time
+
+from ..database import get_session
+from ..config import get_settings
+from ..models import DrsObject
+
 import hashlib
-from datetime import datetime
+from sqlalchemy.exc import IntegrityError
 
 
 settings = get_settings()
@@ -29,38 +28,41 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 
-# def populate_database():
+def populate_database():
 
-files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+    files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
-for file in files:
-    # Get full path
-    file_path = join(dir_path, file)
+    for file in files:
+        # Get full path
+        file_path = join(dir_path, file)
 
-    # Get file size
-    size = getsize(file_path)
+        # Get file size
+        size = getsize(file_path)
 
-    # Get creation time
-    created_time = datetime.fromtimestamp(getctime(file_path)).isoformat()
+        # Get creation time
+        created_time = datetime.fromtimestamp(getctime(file_path)).isoformat()
 
-    # Get md5 checksum
-    checksum_md5 = md5(file_path)
+        # Get md5 checksum
+        checksum_md5 = md5(file_path)
 
-    # Get database
-    db = get_session()
+        # Get database
+        db = get_session()
 
-    # Get downloadable path
-    path = join("https://github.com/ghga-de/sandbox-storage/raw/dev/", file_path)
+        # Get downloadable path
+        path = join("https://github.com/ghga-de/sandbox-storage/raw/dev/", file_path)
 
-    try:
-        drs_object = DrsObject(
-            drs_id=file,
-            path=path,
-            size=size,
-            created_time=created_time,
-            checksum_md5=checksum_md5,
-        )
-        db.add(drs_object)
-        db.flush()
-    except IntegrityError as e:
-        raise e
+        try:
+            # Create Object in Database
+            drs_object = DrsObject(
+                drs_id=file,
+                path=path,
+                size=size,
+                created_time=created_time,
+                checksum_md5=checksum_md5,
+            )
+            db.add(drs_object)
+            db.flush()
+        except IntegrityError as e:
+            raise e
+
+    return
