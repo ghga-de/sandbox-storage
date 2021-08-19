43f4649915fbe4aa5288c7a202e8d66ee813fbc7
MoritzHahn1337 <75744178+MoritzHahn1337@users.noreply.github.com>
2021-08-19 14:12:49 +0200

    Update sandbox_storage/config.py
    Co-authored-by: Kersten Breuer <kersten-breuer@outlook.com>

--- sandbox_storage/config.py
+++ sandbox_storage/config.py
@@ -64,7 +64,6 @@ class Settings(BaseSettings):
     log_level: LogLevel = "info"
     drs_path: str = "drs://localhost:8080/"
     api_path: str = "/ga4gh/drs/v1"
-    example_files_path: str = "sandbox_storage/examples/"
 
 
 def get_settings(
