2afc954ba35f9de496bf32314ab16d8d5c665391
Kersten Breuer <kersten-breuer@outlook.com>
2021-08-23 10:12:48 +0200

    make cors configureable (GDEV-182)
    Make CORS configurable.
    Also migrated the config parsing to the functionality from the shared library.

--- sandbox_storage/pubsub.py
+++ sandbox_storage/pubsub.py
@@ -17,12 +17,12 @@
 
 import pika
 from ghga_service_chassis_lib.pubsub import AmqpTopic
-from .config import get_settings
+from .config import get_config
 
 
 def get_connection_params():
     """Return a configuration object for pika"""
-    config = get_settings()
+    config = get_config()
 
     return pika.ConnectionParameters(
         host=config.rabbitmq_host, port=config.rabbitmq_port
@@ -32,7 +32,7 @@ def get_connection_params():
 def send_message(drs_id: str, access_id: str, user_id: str):
     """Send a message when download request arrives"""
 
-    config = get_settings()
+    config = get_config()
 
     message = {"drs_id": drs_id, "access_id": access_id, "user_id": user_id}
 
