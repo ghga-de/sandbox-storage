09529fd59f5669e5d40831a97833c3a103fdca1f
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-10-18 19:10:16 +0000

    WIP

--- scripts/s3_tests.py
+++ scripts/s3_tests.py
@@ -16,28 +16,89 @@
 # limitations under the License.
 
 import hashlib
+import logging
+import requests
 from datetime import datetime
 from os import listdir
 from os.path import getctime, getsize, isfile, join
 from pathlib import Path
 
 import boto3
-import transaction
-import zope.sqlalchemy
-from sqlalchemy.exc import IntegrityError
+from botocore.exceptions import ClientError
 
 from sandbox_storage.config import get_config
-from sandbox_storage.dao.db import get_session
-from sandbox_storage.dao.db_models import DrsObject
 
 HERE = Path(__file__).parent.resolve()
 DIR_PATH = HERE.parent.resolve() / "examples"
-S3_URL = get_config().s3_url
+S3_URL = "http://s3-localstack:4566"
 
-path = "http://s3-localstack:4566/test/"
 
-# Connect to s3
-s3 = boto3.resource(  # pylint: disable=invalid-name
-    service_name="s3",
-    endpoint_url=S3_URL,
-)
+def s3_connect():
+    # Connect to s3
+    s3 = boto3.client(  # pylint: disable=invalid-name
+        service_name="s3",
+        endpoint_url=S3_URL,
+    )
+
+    return s3
+
+
+def create_bucket(s3, bucket_name):
+    # Create bucket
+    s3_bucket = s3.create_bucket(Bucket=bucket_name)
+
+    return s3_bucket
+
+
+def create_presigned_post(
+    bucket_name, object_name, fields=None, conditions=None, expiration=3600
+):
+    """Generate a presigned URL S3 POST request to upload a file
+
+    :param bucket_name: string
+    :param object_name: string
+    :param fields: Dictionary of prefilled form fields
+    :param conditions: List of conditions to include in the policy
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Dictionary with the following keys:
+        url: URL to post to
+        fields: Dictionary of form fields and values to submit with the POST
+    :return: None if error.
+    """
+
+    # Generate a presigned S3 POST URL
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.generate_presigned_post(
+            bucket_name,
+            object_name,
+            Fields=fields,
+            Conditions=conditions,
+            ExpiresIn=expiration,
+        )
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
+
+
+def run():
+
+    files = [file for file in listdir(DIR_PATH) if isfile(join(DIR_PATH, file))]
+
+    # Generate a presigned S3 POST URL
+    object_name = "OBJECT_NAME"
+    response = create_presigned_post("BUCKET_NAME", object_name)
+    if response is None:
+        exit(1)
+
+    # Demonstrate how another Python program can use the presigned URL to upload a file
+    with open(object_name, "rb") as f:
+        files = {"file": (object_name, f)}
+        http_response = requests.post(
+            response["url"], data=response["fields"], files=files
+        )
+    # If successful, returns HTTP status code 204
+    logging.info(f"File upload HTTP status code: {http_response.status_code}")
